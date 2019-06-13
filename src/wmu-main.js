3d96e61980dfaede3958e7e8d32a8d5f2a9a53b4
SanderPs <sander@studiekring.nl>
2019-06-13 16:29:48 +0200

    Update wmu-main.js

--- src/wmu-main.js
+++ src/wmu-main.js
@@ -1,4 +1,5 @@
 var wmutable = require('./wmu-table');
+var wmuquote = require('./wmu-quote');
 
 const regex_LineEscape = '\\|';
 const regex_Paragraphs = new RegExp( '^(?!' + regex_LineEscape + ')(.+?)(?=$|\\r?\\n\\r?\\n)', 'gm');
@@ -8,14 +9,7 @@ const regex_Bold = /(?:\*\*)(.+)(?:\*\*)/;
 const regex_Italic = /(?:\/\/)(.+)(?:\/\/)/;
 const regex_Underscore = /(?:\_\_)(.+)(?:\_\_)/;
 const regex_Striketrough = /(?:~~)(.+)(?:~~)/;
-const regex_Tablex = /^\|table.*\r?\n(^\|.+\r?\n?)+/gm
-
-
-const eol = "\r\n";
-
-const options = {
-    eol : eol
-};
+const regex_InlineQuote = /(?:"")(.+)(?:"")/;
 
 function transform(x){
     x=x.replace(regex_Paragraphs, '<p>$&</p>');
@@ -25,52 +19,10 @@ function transform(x){
     x=x.replace(regex_Italic, '<i>$1</i>');
     x=x.replace(regex_Underscore, '<u>$1</u>');
     x=x.replace(regex_Striketrough, '<del>$1</del>');
-    x=x.replace(wmutable.regex_Table, wmutable.transformtable)
+    x=x.replace(regex_InlineQuote, '<q>$1</q>');
+    x=x.replace(wmutable.regex_Table, wmutable.transformtable);
+    x=x.replace(wmuquote.regex_Quote, wmuquote.transformquote);
     return x;
-}
-
-function converttablexx(match, p1, p2, p3, offset, string) {
-
-    let item = {
-        type: 'table',
-        header: p1.replace(/^ *| *\| *$/g, '').split(/ *\| */),
-        align: p2.replace(/^ *|\| *$/g, '').split(/ *\| */),
-        cells: p3 ? p3.replace(/\n$/, '').split('\n') : []
-      };
-
-      let result = []
-      result.push("<table>" + eol);
-
-      if (item.header.length === item.align.length) {
-
-        for (i = 0; i < item.align.length; i++) {
-          if (/^ *-+: *$/.test(item.align[i])) {
-            item.align[i] = 'right';
-          } else if (/^ *:-+: *$/.test(item.align[i])) {
-            item.align[i] = 'center';
-          } else if (/^ *:-+ *$/.test(item.align[i])) {
-            item.align[i] = 'left';
-          } else {
-            item.align[i] = null;
-          }
-        }
-
-        result.push("<tr>" + eol);
-        for (i=0; i<item.header.length; i++) {
-            result.push("\t<th" + (item.align[i] ? " align='" + item.align[i] + "'" : "") + ">" + item.header[i] + "</th>" + eol);
-        }
-        result.push("</tr>" + eol);
-      }
-    
-      for (i = 0; i < item.cells.length; i++) {
-        result.push( 
-            (
-                item.cells[i].replace(/^\|(.+)\|$/gm, '<tr>' + eol + '\t<td>$1</td>' + eol + '</tr>' + eol)
-            ).replace(/\|/g, '</td>' + eol + '\t<td>')
-        );
-      }
-      result.push('</table>' + eol);
-    return result.join('');
 }
 
 module.exports = {
